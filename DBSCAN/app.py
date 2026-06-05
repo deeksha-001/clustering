12a81ea09040fe9339cc9156dfef5a7aae30a5c8
deeksha-001 <23B81A7215@cvr.ac.in>
2026-06-05 13:23:51 +0530

    fixed path issues

--- DBSCAN/app.py
+++ DBSCAN/app.py
@@ -23,17 +23,29 @@ df = pd.read_csv(DATA_PATH)
 # Load Saved Files
 # ----------------------------------
 
-BASE_DIR = Path(__file__).resolve().parent
-MODELS_DIR = BASE_DIR.parent / "models"
-
+"""
 cluster_centers = joblib.load(
     MODELS_DIR / "cluster_centers.pkl"
 )
 
 clustered_df = joblib.load(
     MODELS_DIR / "dbscan_clustered_df.pkl"
-)
+) 
+"""
 
+from pathlib import Path
+import streamlit as st
+
+BASE_DIR = Path(__file__).resolve().parent
+MODELS_DIR = BASE_DIR / "models"
+
+st.write("BASE_DIR:", BASE_DIR)
+st.write("MODELS_DIR:", MODELS_DIR)
+st.write("MODELS EXISTS:", MODELS_DIR.exists())
+
+if MODELS_DIR.exists():
+    st.write("FILES:")
+    st.write([f.name for f in MODELS_DIR.iterdir()])
 # ----------------------------------
 # Page Config
 # ----------------------------------
